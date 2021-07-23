d6f29e57c74e275a690214ede40122401f492373
Tom Bassine <tvbassine@gmail.com>
2021-07-23 07:51:56 -0700

    Adding some folders for organization.

---
+++ data_cleaning/clean_data.R
@@ -0,0 +1,6 @@
+# Purpose: Clean the American time use tables.
+# Date: 7/23/21
+#################################################
+
+# Read in the first table:
+x = read.csv('https://raw.githubusercontent.com/tvbassine/american-time-use/master/American%20Time%20Use%20Survey%20-%20table_1.csv')
